4c716464e1b06c1dc092fd4dfa92e7212444d969
Logan <julian@jollycorp.com>
2020-06-18 16:39:14 +0800

    Initial commit

---
+++ src/main/java/org/logan/java/TestJava.java
@@ -0,0 +1,16 @@
+package org.logan.java;
+
+/**
+ * desc: TODO <br/>
+ * time: 2020/6/18 4:20 下午 <br/>
+ * author: Logan <br/>
+ * since V 1.0 <br/>
+ */
+
+class TestJava {
+
+	public static void main(String[] args) {
+		System.out.println("======");
+	}
+
+}
